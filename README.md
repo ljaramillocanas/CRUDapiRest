0849b8808a3975013b9cdeabaff94b07128c6533
ljaramillocanas <ljaramillocanas@gmail.com>
2022-04-02 16:11:26 -0500

    PROYECTO TERMINADO

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Parcial_tdea por Luis Fernando Jaramillo
 Parcial de tendencias de desarrollo.
 
+1- creamos una carpeta en escritorio llamada PARCIAL_TDEA
+2- en el visual studio code abrimos la carpeta y creamos el entorno virtual con el comando
 
-Creamos un entorno virtual 
+python -m venv myenv  
+
+3- activamos el entorno virtual con el comando
+
+ env\Scripts\activate.bat
+
+
+luego revisas el pip list: para revisar que tenemos instalados 
+
+con el comando pip install Flask flask flask_mysqldb instalamos las librerias del framework Flask
+
+y procedemos con la ejecución del codigo, es importante aclarar que se utilizo base de datos MySQL y se utilizo el servidor XAMPP.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from distutils.log import debug
 from operator import methodcaller
@@ -68,81 +67,41 @@ def registrarcurso() :
 
         return jsonify({'Mensaje':"error"})
 
-def pag_no_encontrada(error):
-    return "<h1>LA PAGINA QUE INTENTAS BUSCAR NO EXISTE ! </h1>",404
 
-if __name__ == '__main__':
-    app.config.from_object('config.DevelopmentConfig')
-    app.register_error_handler(404,pag_no_encontrada)
-=======
-
-from distutils.log import debug
-from operator import methodcaller
-import MySQLdb
-from flask import Flask, jsonify, request
-from config import config
-from flask_mysqldb import MySQL
-
-
-app=Flask(__name__)
-
-conexion = MySQL(app)
-
-@app.route('/cursos') #Metodo GET
-def listar_cursos():
-
-    try:
-        cursor = conexion.connection.cursor()
-        sql = "SELECT * FROM curso"
-        cursor.execute(sql)
-        datos = cursor.fetchall()
-        cursos= []
-        for fila in datos:
-            curso ={'codigo':fila[0],'creditos':fila[1],'nombre':fila[2]}
-            cursos.append(curso)
-        print(datos)
-        return jsonify({'cursos':cursos,'Mensaje':"Cursos listados"})
-
-    except Exception as ex:
-            return jsonify({'Mensaje':"error"})
-
-@app.route('/cursos/<codigo>', methods = ['GET']) #MetodoGET para un solo curso
-def read_curso(codigo):
-
-    try:
+@app.route('/cursos/<codigo>', methods = ['PUT'])
+def actualizar_curso(codigo):
     
-        cursor = conexion.connection.cursor()
-        sql = "SELECT * FROM curso WHERE codigo = '{0}'".format(codigo)
-        cursor.execute(sql)
-        datos = cursor.fetchone()
-        if datos != None:
-
-            curso ={'codigo':datos[0],'creditos':datos[1],'nombre':datos[2]}
-            return jsonify({'cursos':curso,'Mensaje':"Curso listado"})
-        else:
-            return jsonify({'mensaje':"Curso no encontrado"})
- 
-    except Exception as ex:
-        
-        return jsonify({'Mensaje':"error"})
-
-@app.route('/cursos', methods=['POST'])
-def registrarcurso() : 
-
     try:
-
-
         cursor = conexion.connection.cursor()
-        sql = """INSERT INTO curso (codigo, creditos,nombre) 
-        VALUES ('{0}',{1},'{2}')""".format(request.json['codigo'],request.json['creditos'],request.json['nombre'],)
+        sql = """UPDATE curso SET creditos = {0},nombre = '{1}' 
+        WHERE codigo = '{2}'""".format(request.json['creditos'],request.json['nombre'],codigo)
         cursor.execute(sql)
         conexion.connection.commit()
-        return jsonify({'mensaje': "Curso registrado"})
+        return jsonify({'mensaje': "Curso actualizado"})
+   
+    except Exception as ex:
+
+        return jsonify({'Mensaje':"error"})
+    
+    
+
+
+@app.route('/cursos/<codigo>', methods = ['DELETE'])
+def eliminar_curso(codigo):
+    
+    try:
+
+        cursor = conexion.connection.cursor()
+        sql = "DELETE FROM curso WHERE codigo = '{0}'".format(codigo)
+        cursor.execute(sql)
+        conexion.connection.commit()
+        return jsonify({'mensaje': "Curso eliminado"})
 
 
     except Exception as ex:
 
         return jsonify({'Mensaje':"error"})
+
 
 def pag_no_encontrada(error):
     return "<h1>LA PAGINA QUE INTENTAS BUSCAR NO EXISTE ! </h1>",404
@@ -150,5 +109,4 @@ def pag_no_encontrada(error):
 if __name__ == '__main__':
     app.config.from_object('config.DevelopmentConfig')
     app.register_error_handler(404,pag_no_encontrada)
->>>>>>> 8c1184f2402ea787b614408f839c4edd1651c081
     app.run( )
